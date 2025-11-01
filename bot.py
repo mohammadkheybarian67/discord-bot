fff226898e04f42a13d6b51e66bf4dba41c7fc96
mohammadkheybarian67 <mohammadkheybarian67@gmail.com>
2025-11-01 23:28:39 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import discord
-import os
 from discord.ext import commands
 
-TOKEN =os.getenv("MTQzNDI1NzMxMTcxNDY0Mzk2OA.GzTJsG.HFD4nKqvvpN231g6UHt9u6V_6k9Hm9X5DFkbIE")
+TOKEN ="MTQzNDI1NzMxMTcxNDY0Mzk2OA.GzTJsG.HFD4nKqvvpN231g6UHt9u6V_6k9Hm9X5DFkbIE"
 bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
 @bot.event
 async def on_ready():
@@ -11,4 +10,5 @@ async def on_ready():
 @bot.command()
 async def سلام(ctx):
     await ctx.send("سلام!حالت چطوره؟")
+
 bot.run(TOKEN)
